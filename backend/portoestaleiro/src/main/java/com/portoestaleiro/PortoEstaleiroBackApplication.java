628a50dae9414907d9ce4a963b166de0027583cf
l4b1r1nto <pedro.bcbacellar@gmail.com>
2023-06-27 19:10:25 -0400

    Inital app base commit

---
+++ backend/portoestaleiro/src/main/java/com/portoestaleiro/PortoEstaleiroBackApplication.java
@@ -0,0 +1,13 @@
+package com.portoestaleiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortoEstaleiroBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortoEstaleiroBackApplication.class, args);
+	}
+
+}
